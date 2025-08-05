c170cf0a142569446116267ae16688cf05299c68
SuleymanDD <sdemirel.2375@gmail.com>
2025-08-05 20:09:29 +0300

    Deneme file changed

--- Deneme/script.js
+++ Deneme/script.js
@@ -1,17 +1,8 @@
-
-let nums = [10,9,9,8,8,11,8,0,9,1]
-console.log(fune(nums))
-
-function fune(nums) {
-    nums.sort()
-
-    let lent=nums.length-1
-    let maxItem=nums[lent]
-    
-    for(let i=lent;i>-1;i--){
-        if(maxItem > nums[i]){
-            return nums[i]
-        }
-    }
-    
+let laptop1={
+brand: "Casper",
+model: "G770",
+publish: "2021",
+price: 25000
 }
+
+let {brand: marka, model, publish, price: fiyat} = laptop1
